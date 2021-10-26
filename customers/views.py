59f7cd206cf7c657e6661cd327a7236dcc96c7e7
seanquinn781 <seanquinn781@gmail.com>
2021-10-26 08:34:53 -0400

    rm unused imports

--- customers/views.py
+++ customers/views.py
@@ -3,7 +3,6 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
 from django.shortcuts import render
-from django.http import HttpResponse, JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework.parsers import JSONParser
 
